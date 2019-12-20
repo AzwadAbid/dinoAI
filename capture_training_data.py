28bd2bbe5a933e4c932445a58c9d16b54bc1631d
Azwad Abid <38776619+AzwadAbid@users.noreply.github.com>
2019-12-20 19:19:06 +0800

    Update capture_training_data.py

--- capture_training_data.py
+++ capture_training_data.py
@@ -2,20 +2,21 @@ from selenium import webdriver
 import selenium.webdriver.common.keys as Keys
 import time
 import threading
-from capture import capture_feed
- 
-# change this to point to chromedriver location
-driver = webdriver.Chrome('/Users/owner/Desktop/all/tools/chromedriver')
+from capture import capture_feed 
 
-# internet connection must be off
-driver.get('http://www.google.com/')
+driver = webdriver.Chrome("./chromedriver.exe")
+options = webdriver.ChromeOptions()
+options.add_experimental_option('excludeSwitches', ['enable-logging'])
+
+driver.get(('chrome://dino/'))
 time.sleep(2)
 page = driver.find_element_by_class_name('offline')
 page.send_keys(u'\ue00d')
 
 capture_feed.start()
 
-while True:
+try:
+    while True:
+        pass
+except KeyboardInterrupt:
     pass
-
-
