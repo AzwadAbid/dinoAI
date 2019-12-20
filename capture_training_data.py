8fc4b76ca1bec5526b4def97346085900ce67f20
Azwad Abid <38776619+AzwadAbid@users.noreply.github.com>
2019-12-20 19:35:44 +0800

    Update capture_training_data.py

--- capture_training_data.py
+++ capture_training_data.py
@@ -2,8 +2,20 @@ from selenium import webdriver
 import selenium.webdriver.common.keys as Keys
 import time
 import threading
-from capture import capture_feed 
+from capture import capture_feed
+import os, shutil
 
+folder = './images/'
+for filename in os.listdir(folder):
+    file_path = os.path.join(folder, filename)
+    try:
+        if os.path.isfile(file_path) or os.path.islink(file_path):
+            os.unlink(file_path)
+        elif os.path.isdir(file_path):
+            shutil.rmtree(file_path)
+    except Exception as e:
+        print('Failed to delete %s. Reason: %s' % (file_path, e))
+ 
 driver = webdriver.Chrome("./chromedriver.exe")
 options = webdriver.ChromeOptions()
 options.add_experimental_option('excludeSwitches', ['enable-logging'])
